34964b3da70d39e1e35e84828847858de9b420d7
yorlysoro <yorlysoro@gmail.com>
2023-03-11 08:35:14 -0400

    first awesome dashbard for odoo

---
+++ models/res_partner.py
@@ -0,0 +1,14 @@
+from odoo import fields, models, api
+
+
+class ResPartner(models.Model):
+    _name = 'res.partner'
+    _inherit = 'res.partner'
+
+    order_ids = fields.One2many('awesome_tshirt.order', 'customer_id', string="Orders")
+    has_active_order = fields.Boolean(compute='_compute_has_active_order', store=True)
+
+    @api.depends('order_ids', 'order_ids.state')
+    def _compute_has_active_order(self):
+        for record in self:
+            record.has_active_order = record.order_ids.filtered(lambda r: r.state not in ['sent', 'cancelled'])
